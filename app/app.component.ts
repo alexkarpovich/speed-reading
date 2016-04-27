858011624d38f6a7d1d308244ded0647611af5ce
Pavel Sosunkevich <pavel.sosunkevich@itechart-group.com>
2016-04-27 13:39:45 +0300

    Initial angular2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,22 @@
-import {Component} from 'angular2/core';
+import {Component} from 'angular2/core'
+import {ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router'
+import {SchulteTable} from './schulte-table/components/schulte-table'
 
 @Component({
-    selector: 'my-app',
-    template: `<h1>My First Angular 2 App</h1>`
+    selector: 'app',
+    template: `
+    	<header>
+    		<nav>
+    			<a [routerLink]="['SchulteTable']">Schulte Table</a>
+    		</nav>
+    	</header>
+    	<div class="main">
+    		<router-outlet></router-outlet>
+    	</div>
+    `,
+    directives: [ROUTER_DIRECTIVES]
 })
-
+@RouteConfig([
+	{path: '/schulte-table', name: 'SchulteTable', component: SchulteTable}
+])
 export class AppComponent {}
---
+++ app/schulte-table/components/schulte-table.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core'
+
+@Component({
+	selector: 'schulte-table',
+	template: `
+		<h2>Hello</h2>
+	`
+})
+export class SchulteTable {
+
+}
