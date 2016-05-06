e7cef18456ab145f40fd633b0fafe776b841e498
Aliaksandr Karpovich <aliaksandr.karpovich@itechart-group.com>
2016-05-06 16:10:55 +0300

    changes

--- app/navigation/components/menu.ts
+++ app/navigation/components/menu.ts
@@ -1,6 +1,6 @@
 import './menu.less'
 import {Component, ViewChild} from 'angular2/core'
-import {ROUTER_DIRECTIVES} from 'angular2/router'
+import {ROUTER_DIRECTIVES, Router} from 'angular2/router'
 
 declare var Snap:any
 declare var mina:any
@@ -15,15 +15,17 @@ declare var mina:any
 export class Menu {
 	@ViewChild('topNav') topNav:any;
 	private items = [
-		{key: 'schulte-table', title: 'Schulte Table', link: '/schulte-table'},
-		{key: 'spinning-number', title: 'Spinning numbers', link: '/spinning-numbers'},
-		{key: 'running-text', title: 'Running Text', link: '/running-text'},
-		{key: 'deconcentration', title: 'Deconcentration', link: '/deconcentration'},
-		{key: 'first', title: 'Schulte Table', link: '/schulte-table'},
-		{key: 'second', title: 'Schulte Table', link: '/schulte-table'},
+		{key: 'schulte-table', title: 'Schulte Table', routeName: 'SchulteTable'},
+		{key: 'spinning-number', title: 'Spinning numbers', routeName: 'SpinningNumbers'},
+		{key: 'running-text', title: 'Running Text', routeName: 'RunningText'},
+		{key: 'deconcentration', title: 'Deconcentration', routeName: 'Deconcentration'},
+		{key: 'first', title: 'Schulte Table', routeName: 'First'},
+		{key: 'second', title: 'Schulte Table', routeName: 'Second'},
 	];
 	private boxSize = 100;
 	private spaceSize = 10;
+
+	constructor(private router: Router) {}
 
 	ngAfterViewInit() {	
 		let topNavSnap = new Snap(this.topNav.nativeElement);
@@ -49,26 +51,43 @@ export class Menu {
 	}
 
 	initRect(topNavSnap, group, item, x, y, shadow) {
+		let boxGroup = group.group();
 		let box = Snap().rect(
 			x*(this.boxSize+this.spaceSize), 
 			y*(this.boxSize+this.spaceSize), 
 			this.boxSize, 
 			this.boxSize
 		);
+		let title = Snap().text(x*(this.boxSize+this.spaceSize), y*(this.boxSize+this.spaceSize) + 100, item.title);
 
+		boxGroup.append(box);
+		boxGroup.append(title);
 		box.attr({
 			id: item.key,
+			class: 'menu-item',
 			fill: '#197700',
 			filter: shadow
 		});
+		title.attr({
+			opacity: '0'
+		});
 
 		box.hover(() => {
-			group.paper.append(box);
-			box.animate({transform: 's1.5,1.5'}, 500, mina.elastic);
+			group.append(boxGroup);			
+			boxGroup.animate({transform: 's1.5,1.5'}, 500, mina.elastic);
+			title.attr({
+				opacity: '1'
+			});
 		}, () => {
-			box.animate({transform: 's1,1'}, 500, mina.elastic);
+			boxGroup.animate({transform: 's1,1'}, 500, mina.elastic);
+			title.attr({
+				opacity: '0'
+			});
 		});
-		
-		group.append(box);
+
+		box.click(() => {
+			boxGroup.animate({transform: 's1.3,1.3'}, 500, mina.elastic);
+			this.router.navigate([item.routeName]);
+		});
 	}
 }
